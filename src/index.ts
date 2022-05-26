ede4e4c0637e3f5387e857b9e76a527fc05dfdc2
Stefan Probst <stefanprobst@posteo.de>
2022-05-26 19:02:23 +0200

    feat: add strictly types object helpers

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+type Keys<T> = Array<keyof T>
+
+export const keys = Object.keys as <T extends object>(obj: T) => Keys<T>
+
+
+type Values<T> = Array<
+  {
+    [K in keyof T]: T[K]
+  }[keyof T]
+>
+
+export const values = Object.values as <T extends object>(obj: T) => Values<T>
+
+type Entries<T> = Array<
+  {
+    [K in keyof T]: [K, T[K]]
+  }[keyof T]
+>
+
+export const entries = Object.entries as <T extends object>(obj: T) => Entries<T>
